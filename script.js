cb1874a2f46374ae9df2f019f5fc52912814a528
noblefx0 <henrynwamba0@gmail.com>
2026-01-19 23:21:16 +0000

    Enhance number formatting in formatNumber function  to include billions

--- script.js
+++ script.js
@@ -134,7 +134,9 @@ document.getElementById("caInput").addEventListener("keypress", (e) => {
 });
 
 function formatNumber(num) {
-  if (num >= 1000000) {
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(2) + 'B';
+  } else if (num >= 1000000) {
     return (num / 1000000).toFixed(2) + 'M';
   } else if (num >= 1000) {
     return (num / 1000).toFixed(2) + 'K';
